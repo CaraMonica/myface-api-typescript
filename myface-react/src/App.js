dbdc01382ca9f06222d07bd96d26d5cb9fa4af3d
caramonica <caramonica@hotmail.co.uk>
2021-09-08 10:44:05 +0100

    Changed from CSS to SCSS

--- myface-react/src/App.js
+++ myface-react/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import PostList from "./Components/PostList";
 import UserList from "./Components/UserList";
 import Header from "./Components/Header";
