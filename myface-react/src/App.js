ddd5ebec6730ceca34e07d5f7199ca36f5a8f78a
caramonica <caramonica@hotmail.co.uk>
2021-09-08 16:17:05 +0100

    Added Router components and navigation between pages Co-authored-by: lollypotter <lollypotter@users.noreply.github.com>

--- myface-react/src/App.js
+++ myface-react/src/App.js
@@ -3,14 +3,24 @@ import "./Styling/App.scss";
 import PostList from "./Components/Posts/PostList";
 import UserList from "./Components/User/UserList";
 import Navbar from "./Components/Header/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <UserList />
-    </div>
-  );
-};
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/users">
+        <Navbar />
+        <UserList />
+      </Route>
+      <Route path="/posts">
+        <Navbar />
+        <PostList />
+      </Route>
+      <Route path="">
+        <div>Sorry - that page doesn't exist.</div>
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
