97f8696bd1fdb7d147f6848d7c3d2acac38080c8
caramonica <caramonica@hotmail.co.uk>
2021-09-08 11:12:46 +0100

    Added users posts

--- myface-react/src/Components/User.js
+++ myface-react/src/Components/User.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Post from "./Post";
+import UserCoverImage from "./UserCoverImage";
 
 const User = ({ user }) => (
   <li className="user post-item">
-    <img className="cover-img" src={user.coverImageUrl} alt={`${user.username} cover`} />
+    <UserCoverImage coverImageUrl={user.coverImageUrl} username={user.username} />
     <div className="table-row">
       <div className="profile-img-container">
         <img className="profile-img" src={user.profileImageUrl} alt={`${user.username} profile`} />
@@ -12,6 +14,12 @@ const User = ({ user }) => (
         <div className="post-info post-date">{user.email}</div>
       </div>
     </div>
+    Posts
+    <ul className="flex-container user-posts">
+      {user.posts.map(post => (
+        <Post post={post} isOnPostPage={false}/>
+      ))}
+    </ul>
   </li>
 );
 
