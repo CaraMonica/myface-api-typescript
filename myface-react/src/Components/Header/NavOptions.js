5ea5cf99ad7318ac8b3d688b538905d35570e628
caramonica <caramonica@hotmail.co.uk>
2021-09-08 16:17:24 +0100

    NavOptions now link to page

--- myface-react/src/Components/Header/NavOptions.js
+++ myface-react/src/Components/Header/NavOptions.js
@@ -3,8 +3,8 @@ import NavOption from "./NavOption";
 
 const NavOptions = ({ click, closeMobileMenu }) => (
   <ul className={click ? "nav-options active" : "nav-options"}>
-    <NavOption onClick={closeMobileMenu} href={"#"} text={"Posts"} isMobileOnly={false} />
-    <NavOption onClick={closeMobileMenu} href={"#"} text={"Users"} isMobileOnly={false} />
+    <NavOption onClick={closeMobileMenu} href={"/posts"} text={"Posts"} isMobileOnly={false} />
+    <NavOption onClick={closeMobileMenu} href={"/users"} text={"Users"} isMobileOnly={false} />
     <NavOption onClick={closeMobileMenu} href={"#"} text={"Create Post"} isMobileOnly={false} />
   </ul>
 );
