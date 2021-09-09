e62a91641db4c553c51a265d14d35df8c2677a35
caramonica <caramonica@hotmail.co.uk>
2021-09-09 18:58:27 +0100

    links to user page

--- myface-react/src/Components/Header/Logo.jsx
+++ myface-react/src/Components/Header/Logo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => (
   <div className="logo-container">
-    <a className="logo" href="#">MyFace</a>
+    <a className="logo" href="/users/31">MyFace</a>
   </div>
 );
 
