5dbd7782cff8ebc50cc8abc1c1e6a51b15982404
caramonica <caramonica@hotmail.co.uk>
2021-09-09 18:40:40 +0100

    Fixed issue with not loading

--- myface-react/src/Components/User/UserPage.jsx
+++ myface-react/src/Components/User/UserPage.jsx
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import User from "./User";
 
-const UserPage = ({ match, location }) => {
-    const [user, setUser] = useState(undefined)
+const UserPage = ({ match }) => {
+  const [userList, setUserList] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:3001/users")
-            .then(response => response.json())
-            .then(data => setUser(data.results.find(({ userId }) => userId === match.params.userId)))
-    }, [match.params.userId]);
+  useEffect(() => {
+    fetch("http://localhost:3001/users")
+      .then(response => response.json())
+      .then(data => setUserList(data.results));
+  }, []);
 
-    return user ? <User user={user} /> : <div> user not found </div>
-}
+  const user = userList.find(({ id }) => id === parseInt(match.params.userId));
+  return user ? <User user={user} /> : <div> user not found </div>
+};
 
 export default UserPage;
